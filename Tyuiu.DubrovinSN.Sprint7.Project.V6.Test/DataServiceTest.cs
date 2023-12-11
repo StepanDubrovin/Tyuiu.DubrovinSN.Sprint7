2da1f385c4954df797a79df2ac66a2114eca6a96
Tyuiu.DubrovinSN@outlook.com <stepan.09myromec@gmail.com>
2023-12-12 00:31:08 +0500

    Add project files.

---
+++ Tyuiu.DubrovinSN.Sprint7.Project.V6.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.DubrovinSN.Sprint7.Project.V6.Lib;
+
+namespace Tyuiu.DubrovinSN.Sprint7.Project.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.DubrovinSN.Sprint7.Project.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DubrovinSN.Sprint7.Project.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DubrovinSN.Sprint7.Project.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7fa37f8f-6f00-404b-bf58-36c609232377")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
