ea8fb8e94096678b8a1c732222f65cde1d6636a0
Tyuiu.DubrovinSN@outlook.com <stepan.09myromec@gmail.com>
2023-12-14 00:55:05 +0500

    Project

--- Tyuiu.DubrovinSN.Sprint7.Project.V6/FormPacient.Designer.cs
+++ Tyuiu.DubrovinSN.Sprint7.Project.V6/FormPacient.Designer.cs
@@ -29,13 +29,53 @@ namespace Tyuiu.DubrovinSN.Sprint7.Project.V6
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelLeft_DSN = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panelLeft_DSN
+            // 
+            this.panelLeft_DSN.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeft_DSN.Location = new System.Drawing.Point(0, 0);
+            this.panelLeft_DSN.Name = "panelLeft_DSN";
+            this.panelLeft_DSN.Size = new System.Drawing.Size(187, 612);
+            this.panelLeft_DSN.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(187, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(581, 612);
+            this.panel1.TabIndex = 1;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(768, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 612);
+            this.splitter1.TabIndex = 2;
+            this.splitter1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(771, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(603, 612);
+            this.panel2.TabIndex = 3;
             // 
             // FormPacient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(944, 563);
+            this.ClientSize = new System.Drawing.Size(1374, 612);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelLeft_DSN);
             this.Name = "FormPacient";
             this.Text = "Пациенты";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
@@ -44,5 +84,10 @@ namespace Tyuiu.DubrovinSN.Sprint7.Project.V6
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelLeft_DSN;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
