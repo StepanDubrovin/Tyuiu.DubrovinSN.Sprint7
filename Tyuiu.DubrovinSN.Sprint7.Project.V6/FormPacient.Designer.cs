6bb9ffa64e1510cab72f2becc6cc1a2f1e5e5b62
Tyuiu.DubrovinSN@outlook.com <stepan.09myromec@gmail.com>
2023-12-14 01:44:51 +0500

    Project

--- Tyuiu.DubrovinSN.Sprint7.Project.V6/FormPacient.Designer.cs
+++ Tyuiu.DubrovinSN.Sprint7.Project.V6/FormPacient.Designer.cs
@@ -29,65 +29,220 @@ namespace Tyuiu.DubrovinSN.Sprint7.Project.V6
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelUp_DSN = new System.Windows.Forms.Panel();
             this.panelLeft_DSN = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.splitter1 = new System.Windows.Forms.Splitter();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelFuncIn_DSN = new System.Windows.Forms.Panel();
+            this.splitter = new System.Windows.Forms.Splitter();
+            this.panelMiddle_DSN = new System.Windows.Forms.Panel();
+            this.groupBoxButtons_DSN = new System.Windows.Forms.GroupBox();
+            this.buttonDelete_DSN = new System.Windows.Forms.Button();
+            this.buttonFunction_DSN = new System.Windows.Forms.Button();
+            this.buttonSaveFile_DSN = new System.Windows.Forms.Button();
+            this.buttonOpenFile_DSN = new System.Windows.Forms.Button();
+            this.groupBoxSearch_DSN = new System.Windows.Forms.GroupBox();
+            this.textBox_SearchText_DSN = new System.Windows.Forms.TextBox();
+            this.label_Ser_DSN = new System.Windows.Forms.Label();
+            this.groupBoxSort_DSN = new System.Windows.Forms.GroupBox();
+            this.labelSor_DSN = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panelUp_DSN.SuspendLayout();
+            this.panelLeft_DSN.SuspendLayout();
+            this.groupBoxButtons_DSN.SuspendLayout();
+            this.groupBoxSearch_DSN.SuspendLayout();
+            this.groupBoxSort_DSN.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panelUp_DSN
+            // 
+            this.panelUp_DSN.BackColor = System.Drawing.SystemColors.Control;
+            this.panelUp_DSN.Controls.Add(this.groupBoxSort_DSN);
+            this.panelUp_DSN.Controls.Add(this.groupBoxSearch_DSN);
+            this.panelUp_DSN.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelUp_DSN.Location = new System.Drawing.Point(0, 0);
+            this.panelUp_DSN.Name = "panelUp_DSN";
+            this.panelUp_DSN.Size = new System.Drawing.Size(1374, 153);
+            this.panelUp_DSN.TabIndex = 0;
             // 
             // panelLeft_DSN
             // 
+            this.panelLeft_DSN.Controls.Add(this.groupBoxButtons_DSN);
             this.panelLeft_DSN.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelLeft_DSN.Location = new System.Drawing.Point(0, 0);
+            this.panelLeft_DSN.Location = new System.Drawing.Point(0, 153);
             this.panelLeft_DSN.Name = "panelLeft_DSN";
-            this.panelLeft_DSN.Size = new System.Drawing.Size(187, 612);
-            this.panelLeft_DSN.TabIndex = 0;
+            this.panelLeft_DSN.Size = new System.Drawing.Size(100, 459);
+            this.panelLeft_DSN.TabIndex = 1;
             // 
-            // panel1
+            // panelFuncIn_DSN
             // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(187, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(581, 612);
-            this.panel1.TabIndex = 1;
+            this.panelFuncIn_DSN.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelFuncIn_DSN.Location = new System.Drawing.Point(100, 153);
+            this.panelFuncIn_DSN.Name = "panelFuncIn_DSN";
+            this.panelFuncIn_DSN.Size = new System.Drawing.Size(625, 459);
+            this.panelFuncIn_DSN.TabIndex = 2;
             // 
-            // splitter1
+            // splitter
             // 
-            this.splitter1.Location = new System.Drawing.Point(768, 0);
-            this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(3, 612);
-            this.splitter1.TabIndex = 2;
-            this.splitter1.TabStop = false;
+            this.splitter.Location = new System.Drawing.Point(725, 153);
+            this.splitter.Name = "splitter";
+            this.splitter.Size = new System.Drawing.Size(3, 459);
+            this.splitter.TabIndex = 3;
+            this.splitter.TabStop = false;
             // 
-            // panel2
+            // panelMiddle_DSN
             // 
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(771, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(603, 612);
-            this.panel2.TabIndex = 3;
+            this.panelMiddle_DSN.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMiddle_DSN.Location = new System.Drawing.Point(728, 153);
+            this.panelMiddle_DSN.Name = "panelMiddle_DSN";
+            this.panelMiddle_DSN.Size = new System.Drawing.Size(646, 459);
+            this.panelMiddle_DSN.TabIndex = 4;
+            // 
+            // groupBoxButtons_DSN
+            // 
+            this.groupBoxButtons_DSN.Controls.Add(this.buttonDelete_DSN);
+            this.groupBoxButtons_DSN.Controls.Add(this.buttonFunction_DSN);
+            this.groupBoxButtons_DSN.Controls.Add(this.buttonSaveFile_DSN);
+            this.groupBoxButtons_DSN.Controls.Add(this.buttonOpenFile_DSN);
+            this.groupBoxButtons_DSN.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxButtons_DSN.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxButtons_DSN.Name = "groupBoxButtons_DSN";
+            this.groupBoxButtons_DSN.Size = new System.Drawing.Size(100, 459);
+            this.groupBoxButtons_DSN.TabIndex = 0;
+            this.groupBoxButtons_DSN.TabStop = false;
+            this.groupBoxButtons_DSN.Text = "Инструменты";
+            // 
+            // buttonDelete_DSN
+            // 
+            this.buttonDelete_DSN.Image = global::Tyuiu.DubrovinSN.Sprint7.Project.V6.Properties.Resources.free_icon_clear_3602045;
+            this.buttonDelete_DSN.Location = new System.Drawing.Point(12, 233);
+            this.buttonDelete_DSN.Name = "buttonDelete_DSN";
+            this.buttonDelete_DSN.Size = new System.Drawing.Size(72, 61);
+            this.buttonDelete_DSN.TabIndex = 3;
+            this.buttonDelete_DSN.UseVisualStyleBackColor = true;
+            // 
+            // buttonFunction_DSN
+            // 
+            this.buttonFunction_DSN.Image = global::Tyuiu.DubrovinSN.Sprint7.Project.V6.Properties.Resources.free_icon_graph_8780403;
+            this.buttonFunction_DSN.Location = new System.Drawing.Point(12, 166);
+            this.buttonFunction_DSN.Name = "buttonFunction_DSN";
+            this.buttonFunction_DSN.Size = new System.Drawing.Size(72, 61);
+            this.buttonFunction_DSN.TabIndex = 2;
+            this.buttonFunction_DSN.UseVisualStyleBackColor = true;
+            // 
+            // buttonSaveFile_DSN
+            // 
+            this.buttonSaveFile_DSN.Image = global::Tyuiu.DubrovinSN.Sprint7.Project.V6.Properties.Resources.free_icon_photo_11918718;
+            this.buttonSaveFile_DSN.Location = new System.Drawing.Point(12, 99);
+            this.buttonSaveFile_DSN.Name = "buttonSaveFile_DSN";
+            this.buttonSaveFile_DSN.Size = new System.Drawing.Size(72, 61);
+            this.buttonSaveFile_DSN.TabIndex = 1;
+            this.buttonSaveFile_DSN.UseVisualStyleBackColor = true;
+            // 
+            // buttonOpenFile_DSN
+            // 
+            this.buttonOpenFile_DSN.Image = global::Tyuiu.DubrovinSN.Sprint7.Project.V6.Properties.Resources.free_icon_open_folder_8406407;
+            this.buttonOpenFile_DSN.Location = new System.Drawing.Point(12, 32);
+            this.buttonOpenFile_DSN.Name = "buttonOpenFile_DSN";
+            this.buttonOpenFile_DSN.Size = new System.Drawing.Size(72, 61);
+            this.buttonOpenFile_DSN.TabIndex = 0;
+            this.buttonOpenFile_DSN.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxSearch_DSN
+            // 
+            this.groupBoxSearch_DSN.Controls.Add(this.label_Ser_DSN);
+            this.groupBoxSearch_DSN.Controls.Add(this.textBox_SearchText_DSN);
+            this.groupBoxSearch_DSN.Location = new System.Drawing.Point(12, 26);
+            this.groupBoxSearch_DSN.Name = "groupBoxSearch_DSN";
+            this.groupBoxSearch_DSN.Size = new System.Drawing.Size(189, 101);
+            this.groupBoxSearch_DSN.TabIndex = 0;
+            this.groupBoxSearch_DSN.TabStop = false;
+            this.groupBoxSearch_DSN.Text = "Поиск";
+            // 
+            // textBox_SearchText_DSN
+            // 
+            this.textBox_SearchText_DSN.Location = new System.Drawing.Point(6, 69);
+            this.textBox_SearchText_DSN.Multiline = true;
+            this.textBox_SearchText_DSN.Name = "textBox_SearchText_DSN";
+            this.textBox_SearchText_DSN.Size = new System.Drawing.Size(177, 20);
+            this.textBox_SearchText_DSN.TabIndex = 0;
+            // 
+            // label_Ser_DSN
+            // 
+            this.label_Ser_DSN.AutoSize = true;
+            this.label_Ser_DSN.Location = new System.Drawing.Point(3, 43);
+            this.label_Ser_DSN.Name = "label_Ser_DSN";
+            this.label_Ser_DSN.Size = new System.Drawing.Size(158, 13);
+            this.label_Ser_DSN.TabIndex = 1;
+            this.label_Ser_DSN.Text = "Укажите элемент для поиска";
+            // 
+            // groupBoxSort_DSN
+            // 
+            this.groupBoxSort_DSN.Controls.Add(this.textBox1);
+            this.groupBoxSort_DSN.Controls.Add(this.labelSor_DSN);
+            this.groupBoxSort_DSN.Location = new System.Drawing.Point(229, 26);
+            this.groupBoxSort_DSN.Name = "groupBoxSort_DSN";
+            this.groupBoxSort_DSN.Size = new System.Drawing.Size(189, 101);
+            this.groupBoxSort_DSN.TabIndex = 1;
+            this.groupBoxSort_DSN.TabStop = false;
+            this.groupBoxSort_DSN.Text = "Сортировка";
+            // 
+            // labelSor_DSN
+            // 
+            this.labelSor_DSN.AutoSize = true;
+            this.labelSor_DSN.Location = new System.Drawing.Point(6, 43);
+            this.labelSor_DSN.Name = "labelSor_DSN";
+            this.labelSor_DSN.Size = new System.Drawing.Size(154, 13);
+            this.labelSor_DSN.TabIndex = 0;
+            this.labelSor_DSN.Text = "Укажите нужную сортировку";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 69);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 20);
+            this.textBox1.TabIndex = 1;
             // 
             // FormPacient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1374, 612);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.splitter1);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelMiddle_DSN);
+            this.Controls.Add(this.splitter);
+            this.Controls.Add(this.panelFuncIn_DSN);
             this.Controls.Add(this.panelLeft_DSN);
+            this.Controls.Add(this.panelUp_DSN);
             this.Name = "FormPacient";
             this.Text = "Пациенты";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panelUp_DSN.ResumeLayout(false);
+            this.panelLeft_DSN.ResumeLayout(false);
+            this.groupBoxButtons_DSN.ResumeLayout(false);
+            this.groupBoxSearch_DSN.ResumeLayout(false);
+            this.groupBoxSearch_DSN.PerformLayout();
+            this.groupBoxSort_DSN.ResumeLayout(false);
+            this.groupBoxSort_DSN.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Panel panelUp_DSN;
         private System.Windows.Forms.Panel panelLeft_DSN;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Splitter splitter1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panelFuncIn_DSN;
+        private System.Windows.Forms.Splitter splitter;
+        private System.Windows.Forms.Panel panelMiddle_DSN;
+        private System.Windows.Forms.GroupBox groupBoxButtons_DSN;
+        private System.Windows.Forms.Button buttonFunction_DSN;
+        private System.Windows.Forms.Button buttonSaveFile_DSN;
+        private System.Windows.Forms.Button buttonOpenFile_DSN;
+        private System.Windows.Forms.Button buttonDelete_DSN;
+        private System.Windows.Forms.GroupBox groupBoxSearch_DSN;
+        private System.Windows.Forms.Label label_Ser_DSN;
+        private System.Windows.Forms.TextBox textBox_SearchText_DSN;
+        private System.Windows.Forms.GroupBox groupBoxSort_DSN;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelSor_DSN;
     }
 }
--- Tyuiu.DubrovinSN.Sprint7.Project.V6/Properties/Resources.Designer.cs
+++ Tyuiu.DubrovinSN.Sprint7.Project.V6/Properties/Resources.Designer.cs
@@ -73,6 +73,26 @@ namespace Tyuiu.DubrovinSN.Sprint7.Project.V6.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap free_icon_clear_3602045 {
+            get {
+                object obj = ResourceManager.GetObject("free-icon-clear-3602045", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap free_icon_graph_8780403 {
+            get {
+                object obj = ResourceManager.GetObject("free-icon-graph-8780403", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap free_icon_info_943579 {
             get {
                 object obj = ResourceManager.GetObject("free-icon-info-943579", resourceCulture);
@@ -103,6 +123,16 @@ namespace Tyuiu.DubrovinSN.Sprint7.Project.V6.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap free_icon_line_chart_126425 {
+            get {
+                object obj = ResourceManager.GetObject("free-icon-line-chart-126425", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap free_icon_nurse_10342190 {
             get {
                 object obj = ResourceManager.GetObject("free-icon-nurse-10342190", resourceCulture);
@@ -116,6 +146,16 @@ namespace Tyuiu.DubrovinSN.Sprint7.Project.V6.Properties {
         internal static System.Drawing.Bitmap free_icon_open_file_4847498 {
             get {
                 object obj = ResourceManager.GetObject("free-icon-open-file-4847498", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap free_icon_open_folder_8406407 {
+            get {
+                object obj = ResourceManager.GetObject("free-icon-open-folder-8406407", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -146,6 +186,16 @@ namespace Tyuiu.DubrovinSN.Sprint7.Project.V6.Properties {
         internal static System.Drawing.Bitmap free_icon_patient_4465493__2_ {
             get {
                 object obj = ResourceManager.GetObject("free-icon-patient-4465493 (2)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap free_icon_photo_11918718 {
+            get {
+                object obj = ResourceManager.GetObject("free-icon-photo-11918718", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
