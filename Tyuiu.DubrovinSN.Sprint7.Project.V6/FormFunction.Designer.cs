7070a9c564c13e0fff6eecc14ca1a1dfda537f78
Tyuiu.DubrovinSN@outlook.com <stepan.09myromec@gmail.com>
2023-12-23 02:11:12 +0500

    Project

--- Tyuiu.DubrovinSN.Sprint7.Project.V6/FormFunction.Designer.cs
+++ Tyuiu.DubrovinSN.Sprint7.Project.V6/FormFunction.Designer.cs
@@ -33,7 +33,19 @@ namespace Tyuiu.DubrovinSN.Sprint7.Project.V6
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chartFunction_DSN = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.buttonOpenMat_DSN = new System.Windows.Forms.Button();
+            this.buttonSaveGraph_DSN = new System.Windows.Forms.Button();
+            this.dataGridViewInMatGa_DSN = new System.Windows.Forms.DataGridView();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.chartFunction_DSN)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInMatGa_DSN)).BeginInit();
             this.SuspendLayout();
             // 
             // chartFunction_DSN
@@ -45,24 +57,100 @@ namespace Tyuiu.DubrovinSN.Sprint7.Project.V6
             this.chartFunction_DSN.Legends.Add(legend1);
             this.chartFunction_DSN.Location = new System.Drawing.Point(0, 0);
             this.chartFunction_DSN.Name = "chartFunction_DSN";
+            this.chartFunction_DSN.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chartFunction_DSN.Series.Add(series1);
-            this.chartFunction_DSN.Size = new System.Drawing.Size(800, 450);
+            this.chartFunction_DSN.Size = new System.Drawing.Size(612, 712);
             this.chartFunction_DSN.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonSaveGraph_DSN);
+            this.panel1.Controls.Add(this.buttonOpenMat_DSN);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(98, 712);
+            this.panel1.TabIndex = 1;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(98, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 712);
+            this.splitter1.TabIndex = 2;
+            this.splitter1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridViewInMatGa_DSN);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(101, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(651, 712);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.chartFunction_DSN);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(752, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(612, 712);
+            this.panel3.TabIndex = 4;
+            // 
+            // buttonOpenMat_DSN
+            // 
+            this.buttonOpenMat_DSN.Image = global::Tyuiu.DubrovinSN.Sprint7.Project.V6.Properties.Resources.free_icon_open_file_4847498;
+            this.buttonOpenMat_DSN.Location = new System.Drawing.Point(12, 23);
+            this.buttonOpenMat_DSN.Name = "buttonOpenMat_DSN";
+            this.buttonOpenMat_DSN.Size = new System.Drawing.Size(72, 61);
+            this.buttonOpenMat_DSN.TabIndex = 0;
+            this.buttonOpenMat_DSN.UseVisualStyleBackColor = true;
+            this.buttonOpenMat_DSN.Click += new System.EventHandler(this.buttonOpenMat_DSN_Click);
+            // 
+            // buttonSaveGraph_DSN
+            // 
+            this.buttonSaveGraph_DSN.Image = global::Tyuiu.DubrovinSN.Sprint7.Project.V6.Properties.Resources.direct_download;
+            this.buttonSaveGraph_DSN.Location = new System.Drawing.Point(12, 99);
+            this.buttonSaveGraph_DSN.Name = "buttonSaveGraph_DSN";
+            this.buttonSaveGraph_DSN.Size = new System.Drawing.Size(72, 61);
+            this.buttonSaveGraph_DSN.TabIndex = 1;
+            this.buttonSaveGraph_DSN.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewInMatGa_DSN
+            // 
+            this.dataGridViewInMatGa_DSN.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewInMatGa_DSN.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewInMatGa_DSN.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewInMatGa_DSN.Name = "dataGridViewInMatGa_DSN";
+            this.dataGridViewInMatGa_DSN.Size = new System.Drawing.Size(651, 712);
+            this.dataGridViewInMatGa_DSN.TabIndex = 0;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
             // 
             // FormFunction
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.chartFunction_DSN);
+            this.ClientSize = new System.Drawing.Size(1364, 712);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.panel1);
             this.Name = "FormFunction";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "График функции";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.chartFunction_DSN)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInMatGa_DSN)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -70,5 +158,13 @@ namespace Tyuiu.DubrovinSN.Sprint7.Project.V6
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chartFunction_DSN;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonOpenMat_DSN;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button buttonSaveGraph_DSN;
+        private System.Windows.Forms.DataGridView dataGridViewInMatGa_DSN;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
     }
 }
--- Tyuiu.DubrovinSN.Sprint7.Project.V6/FormFunction.cs
+++ Tyuiu.DubrovinSN.Sprint7.Project.V6/FormFunction.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tyuiu.DubrovinSN.Sprint7.Project.V6.Lib;
+
 
 namespace Tyuiu.DubrovinSN.Sprint7.Project.V6
 {
@@ -15,6 +17,39 @@ namespace Tyuiu.DubrovinSN.Sprint7.Project.V6
         public FormFunction()
         {
             InitializeComponent();
+        }
+        string path = @"C:\C#\Tyuiu.DubrovinSN.Sprint7\Tyuiu.DubrovinSN.Sprint7.Project.V6\bin\Debug\Информация о пациентах.csv";
+        static string openFile;
+        static int rows;
+        static int columns;
+        static string[,] matrix;
+        DataService ds = new DataService();
+        private void buttonOpenMat_DSN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog.ShowDialog();
+                openFile = openFileDialog.FileName;
+
+                matrix = ds.GetMatrix(openFile);
+                rows = matrix.GetLength(0);
+                columns = matrix.GetLength(1);
+                dataGridViewInMatGa_DSN.RowCount = rows;
+                dataGridViewInMatGa_DSN.ColumnCount = columns;
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        dataGridViewInMatGa_DSN.Rows[i].Cells[j].Value = matrix[i, j];
+                        dataGridViewInMatGa_DSN.Rows[i].Cells[j].Selected = false;
+                    }
+                }
+                dataGridViewInMatGa_DSN.AutoResizeColumns();
+            }
+            catch
+            {
+                MessageBox.Show("Проблема с открытием файла", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
